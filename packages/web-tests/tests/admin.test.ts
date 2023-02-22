5de81e504d7c9806d25d2899efa4a38e60771d9d
Raphaël Grand <raphael.grand@zenika.com>
2023-02-22 17:50:13 +0100

    :white_check_mark: Add actions to tests

--- packages/web-tests/tests/admin.test.ts
+++ packages/web-tests/tests/admin.test.ts
@@ -3,7 +3,7 @@ import { test, expect } from "@playwright/test";
 const baseURL = "http://localhost:4173";
 
 test.describe("Quiz Admin", () => {
-  test("Create and delete quiz", async ({ context, page }) => {
+  test("Create,start and delete quiz", async ({ context, page }) => {
     // Create a quiz.
     await page.goto(`${baseURL}/#/admin`);
     const input = page.getByPlaceholder("Name your quiz");
@@ -19,16 +19,47 @@ test.describe("Quiz Admin", () => {
     await expect(
       page.getByText(`${baseURL}/#/display/${quizID}`)
     ).toBeVisible();
-    await expect(page.getByText("Start quiz")).toBeVisible();
-    await expect(page.getByText("Delete quiz")).toBeVisible();
-
-    // await page.locator("a").click();
-
-    // Delete a quiz.
-    const adminPage = await context.newPage();
-    await adminPage.goto(`${baseURL}/#/admin/${quizID}`);
+    const startButton = page.getByText("Start quiz");
+    await expect(startButton).toBeVisible();
     const deleteButton = page.getByText("Delete quiz");
     await expect(deleteButton).toBeVisible();
+
+    // User page in order to trigger some events.
+    const userPage = await context.newPage();
+    await userPage.goto(`${baseURL}/#/${quizID}`);
+    await userPage.getByPlaceholder("Chose a name").fill("Bruce Wayne");
+    await userPage.keyboard.press("Enter");
+    await expect(userPage.getByRole("button")).toBeVisible();
+
+    // Start quiz.
+    await startButton.click();
+    const buzzersOnButton = page.getByText("Buzzers ON");
+    const buzzersOffButton = page.getByText("Buzzers OFF");
+    const displayQRButton = page.getByText("Display QR code");
+    const displayTeamsButton = page.getByText("Display teams");
+    const razScoresButton = page.getByText("Raz scores");
+    await expect(buzzersOnButton).toBeVisible();
+    await expect(buzzersOffButton).toBeVisible();
+    await expect(displayQRButton).toBeVisible();
+    await expect(displayTeamsButton).toBeVisible();
+    await expect(razScoresButton).toBeVisible();
+
+    // Buz.
+    await expect(userPage.getByRole("button")).toBeVisible();
+    await userPage.getByRole("button").click();
+
+    // Check if user buzz has appeared in admin screen.
+    await expect(page.getByText("Bruce Wayne")).toBeVisible();
+    const winButton = page.locator(".quiz-winner-button.yes");
+    await expect(winButton).toBeVisible();
+    await winButton.click();
+
+    // Check if point has been given to team.
+    await displayTeamsButton.click();
+    await expect(page.getByText("1")).toBeVisible();
+    await page.getByText("X").click();
+
+    // Delete quiz.
     await deleteButton.click();
     await expect(deleteButton).not.toBeVisible();
   });
